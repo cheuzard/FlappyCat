72133105c9fd54205a8debadddb283a67e3a0e5e
cheuzard <hichem172019@gmail.com>
2026-01-01 23:22:54 +0100

    refactor returned to legacy ressource loading

--- src/util/ResourceLoader.java
+++ src/util/ResourceLoader.java
@@ -1,9 +1,7 @@
 package util;
 
-import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,27 +20,16 @@ public class ResourceLoader {
     }
 
     public Image getImage(String imagePath) {
-        if (imagePath == null) {
-            return null;
-        }
 
         if (imageCache.containsKey(imagePath)) {
             return imageCache.get(imagePath);
         } else {
-            Image img = null;
+            Image img;
             try {
-                // Load image from resources (works in JAR and IDE)
-                InputStream stream = getClass().getResourceAsStream(imagePath);
-                if (stream != null) {
-                    img = ImageIO.read(stream);
-                    stream.close();
-                    imageCache.put(imagePath, img);
-                } else {
-                    System.err.println("Could not find resource: " + imagePath);
-                }
-            } catch (IOException e) {
-                System.err.println("Error loading image: " + imagePath);
-                e.printStackTrace();
+                img = new ImageIcon(imagePath).getImage();
+                imageCache.put(imagePath, img);
+            } catch (Exception e) {
+                img = null;
             }
 
             return img;
